958392bba9ba12ac3f693cab7734cd9bd8dd50ed
gil9red <ilya.petrash@inbox.ru>
2023-04-20 14:12:21 +0500

    Рефакторинг. Использование стиля кода black

--- app_web_server/config.py
+++ app_web_server/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from pathlib import Path
@@ -9,7 +9,7 @@ from pathlib import Path
 
 DIR: Path = Path(__file__).resolve().parent
 
-DIR_LOGS: Path = DIR / 'logs'
+DIR_LOGS: Path = DIR / "logs"
 DIR_LOGS.mkdir(parents=True, exist_ok=True)
 
 PORT_WEB: int = 12000
--- app_web_server/main.py
+++ app_web_server/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import datetime as DT
@@ -20,44 +20,45 @@ def index():
     items = []
     for metal_rate in MetalRate.get_last_rates(number=-1):
         items.append({
-            'date': metal_rate.get_date_title(),
-            'date_iso': metal_rate.date.isoformat(),
-            'gold': float(metal_rate.gold),
-            'silver': float(metal_rate.silver),
-            'platinum': float(metal_rate.platinum),
-            'palladium': float(metal_rate.palladium),
+            "date": metal_rate.get_date_title(),
+            "date_iso": metal_rate.date.isoformat(),
+            "gold": float(metal_rate.gold),
+            "silver": float(metal_rate.silver),
+            "platinum": float(metal_rate.platinum),
+            "palladium": float(metal_rate.palladium),
         })
 
     start_date, end_date = MetalRate.get_range_dates()
     filter_date = end_date - DT.timedelta(days=365)
 
     return render_template(
-        'index.html',
-        title='Цены драгоценных металлов',
+        "index.html",
+        title="Цены драгоценных металлов",
         items=items,
-        start_date=str(start_date), end_date=str(end_date),
+        start_date=str(start_date),
+        end_date=str(end_date),
         filter_date=str(filter_date),
         metals={
             metal.name_lower: {
-                k: v for k, v in vars(metal).items() if not k.startswith('_')
+                k: v for k, v in vars(metal).items() if not k.startswith("_")
             }
             for metal in MetalEnum
         },
     )
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
     return send_from_directory(
-        os.path.join(app.root_path, 'static/images'),
-        'favicon.png'
+        os.path.join(app.root_path, "static/images"),
+        "favicon.png",
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # app.debug = True
 
     app.run(
-        host='0.0.0.0',
-        port=config.PORT_WEB
+        host="0.0.0.0",
+        port=config.PORT_WEB,
     )
