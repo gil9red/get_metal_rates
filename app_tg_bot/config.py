0d1c3815109d28959bf112486899e41c331ae9e5
gil9red <ilya.petrash@inbox.ru>
2023-04-20 14:11:16 +0500

    Рефакторинг. Использование стиля кода black

--- app_tg_bot/config.py
+++ app_tg_bot/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
@@ -13,25 +13,25 @@ from pathlib import Path
 # Текущая папка, где находится скрипт
 DIR = Path(__file__).resolve().parent
 
-DIR_LOGS: Path = DIR / 'logs'
+DIR_LOGS: Path = DIR / "logs"
 DIR_LOGS.mkdir(parents=True, exist_ok=True)
 
-TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
+TOKEN_FILE_NAME = DIR / "TOKEN.txt"
 
 try:
-    TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
+    TOKEN = os.environ.get("TOKEN") or TOKEN_FILE_NAME.read_text("utf-8").strip()
     if not TOKEN:
-        raise Exception('TOKEN пустой!')
+        raise Exception("TOKEN пустой!")
 
 except:
-    print(f'Нужно в {TOKEN_FILE_NAME.name} или в переменную окружения TOKEN добавить токен бота')
+    print(f"Нужно в {TOKEN_FILE_NAME.name} или в переменную окружения TOKEN добавить токен бота")
     TOKEN_FILE_NAME.touch()
     sys.exit()
 
 USER_NAME_ADMINS = [
-    '@ilya_petrash',
+    "@ilya_petrash",
 ]
 
-ERROR_TEXT = 'Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже...'
+ERROR_TEXT = "Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже..."
 
 MAX_MESSAGE_LENGTH = 4096
